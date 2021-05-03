0f3920b3128e7cfac87732104d99cb3f076d979a
WiebevanHerwijnen <32171131+WiebevanHerwijnen@users.noreply.github.com>
2021-05-03 19:09:22 +0200

    Update trendService.js

--- routers/trendService.js
+++ routers/trendService.js
@@ -3,11 +3,24 @@ var router = express.Router()
 const apiAdapter = require('./apiAdapter')
 var jwtAuthz = require('express-jwt-authz');
 
-const BASE_URL = 'http://localhost:8088'
+const BASE_URL = 'http://localhost:3002'
 const api = apiAdapter(BASE_URL)
 
 router.get('/trends', (req, res) => {
   api.get(req.path).then(resp => {
+    res.send(resp.data)
+  })
+})
+
+router.post('/trend', (req, res) => {
+  console.log(req.body)
+  api.post(req.path, req.body).then(resp => {
+    res.send(resp.data)
+  })
+})
+
+router.post('/trend/check', (req, res) => {
+  api.post(req.path, req.body).then(resp => {
     res.send(resp.data)
   })
 })
@@ -17,5 +30,13 @@ router.get('/trend/:trendname', (req, res) => {
     res.send(resp.data)
   })
 })
+
+router.delete('/trend/:trendid', (req, res) => {
+  api.delete(req.path).then(resp => {
+    res.send(resp.data)
+  })
+})
+
+
 
 module.exports = router
